4e97d5cbc2f581215e9ab4e800c738513b70e108
Gustavo-dev-it <tavintwd@gmail.com>
2024-08-28 21:34:05 -0300

    aprendizado react

--- 03-react-fundamentos/src/app/fundamentos/page.js
+++ 03-react-fundamentos/src/app/fundamentos/page.js
@@ -1,16 +1,45 @@
 import MeuComponente from "./Meu Componente.js"
 import NumeroMaior from "./NumeroMaior.js"
+import NumeroAleatorio from "./NumeroAleatorio.js"
+import Cabecalho from "./Cabecalho.js"
+import Familia from "./Familia.js"
+import Filho from "./Filho.js"
+import ImagemAleatoria from "./ImagemAleatoria.js"
 
 
 export default function Fundamentos() {
     return (
         <>
             {/*Comentarios no JSX*/}
-            <h1>MeuComponente1</h1>
-            <hr />
+            <Cabecalho titulo="Fundamentos" descricao="Páginas de fundamentos de React/Next"/>
             <MeuComponente />
             <hr />
             <NumeroMaior numA={2} numB={10} />
+           <hr/>
+
+           <NumeroAleatorio />
+           <NumeroAleatorio />
+           <NumeroAleatorio />
+
+           <hr/>
+
+                <Familia nomeFamilia='soares'>
+
+                <Filho nome="Julia" sobreNome="soares"/>
+                <Filho nome="João" sobreNome="soares"/>
+                <Filho nome="José" sobreNome="soares"/>
+
+           </Familia>
+
+<hr/>
+
+<ImagemAleatoria>
+
+
+
+    
+</ImagemAleatoria>
+
            
         </>
     )
